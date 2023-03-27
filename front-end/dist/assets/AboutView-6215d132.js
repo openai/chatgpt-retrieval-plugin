c101b8bc0e28495f5d3870b2fb931900bc9e9643
kelly <kelly_xu@trendmicro.com>
2023-03-27 15:30:19 +0800

    feat: update git ignore add dist to repo

---
+++ front-end/dist/assets/AboutView-6215d132.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as s}from"./index-c4f8ba4b.js";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function i(r,u){return t(),o("div",c,n)}const l=e(_,[["render",i]]);export{l as default};
