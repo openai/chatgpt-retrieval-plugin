0529d31b6f651dd118d3b4ec3d7c1594dc34cfaa
p20y <heypradeeps@gmail.com>
2023-04-30 13:48:18 -0700

    updated url

--- server/main.py
+++ server/main.py
@@ -37,7 +37,7 @@ sub_app = FastAPI(
     title="Retrieval Plugin API",
     description="A retrieval API for querying and filtering documents based on natural language queries and metadata",
     version="1.0.0",
-    servers=[{"url": "https://your-app-url.com"}],
+    servers=[{"url": "https://monkfish-app-59nbq.ondigitalocean.app/"}],
     dependencies=[Depends(validate_token)],
 )
 app.mount("/sub", sub_app)
