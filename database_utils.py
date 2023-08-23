ffaaf4cf51c4791733d84b5662736fa52be34694
lshaoqin <shaoqin.lu@gmail.com>
2023-08-23 21:48:40 +0800

    implement env

---
+++ database_utils.py
@@ -0,0 +1,81 @@
+from typing import Any, Dict
+import requests
+import os
+
+SEARCH_TOP_K = 3
+DATABASE_INTERFACE_BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+def upsert_file(directory: str):
+    """
+    Upload all files under a directory to the vector database.
+    """
+    url = "http://0.0.0.0:8000/upsert-file"
+    headers = {"Authorization": "Bearer " + DATABASE_INTERFACE_BEARER_TOKEN}
+    files = []
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, "rb") as f:
+                file_content = f.read()
+                files.append(("file", (filename, file_content, "text/plain")))
+            response = requests.post(url,
+                                     headers=headers,
+                                     files=files,
+                                     timeout=600)
+            if response.status_code == 200:
+                print(filename + " uploaded successfully.")
+            else:
+                print(
+                    f"Error: {response.status_code} {response.content} for uploading "
+                    + filename)
+
+
+def upsert(id: str, content: str):
+    """
+    Upload one piece of text to the database.
+    """
+    url = "http://0.0.0.0:8000/upsert"
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + DATABASE_INTERFACE_BEARER_TOKEN,
+    }
+
+    data = {
+        "documents": [{
+            "id": id,
+            "text": content,
+        }]
+    }
+    response = requests.post(url, json=data, headers=headers, timeout=600)
+
+    if response.status_code == 200:
+        print("uploaded successfully.")
+    else:
+        print(f"Error: {response.status_code} {response.content}")
+
+
+def query_database(query_prompt: str) -> Dict[str, Any]:
+    """
+    Query vector database to retrieve chunk with user's input question.
+    """
+    url = "http://0.0.0.0:8000/query"
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "application/json",
+        "Authorization": f"Bearer {DATABASE_INTERFACE_BEARER_TOKEN}",
+    }
+    data = {"queries": [{"query": query_prompt, "top_k": SEARCH_TOP_K}]}
+
+    response = requests.post(url, json=data, headers=headers, timeout=600)
+
+    if response.status_code == 200:
+        result = response.json()
+        # process the result
+        return result
+    else:
+        raise ValueError(f"Error: {response.status_code} : {response.content}")
+
+
+if __name__ == "__main__":
+    upsert_file("./data")
--- services/chunks.py
+++ services/chunks.py
@@ -13,7 +13,7 @@ tokenizer = tiktoken.get_encoding(
 )  # The encoding scheme to use for tokenization
 
 # Constants
-CHUNK_SIZE = 200  # The target size of each text chunk in tokens
+CHUNK_SIZE = 1024  # The target size of each text chunk in tokens
 MIN_CHUNK_SIZE_CHARS = 350  # The minimum size of each text chunk in characters
 MIN_CHUNK_LENGTH_TO_EMBED = 5  # Discard chunks shorter than this
 EMBEDDINGS_BATCH_SIZE = int(os.environ.get("OPENAI_EMBEDDING_BATCH_SIZE", 128))  # The number of embeddings to request at a time
