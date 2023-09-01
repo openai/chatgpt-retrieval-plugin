4d0af837f379d247087f33d3da3e989341ca054a
pmcbride <pmcbride@github.com>
2023-09-01 14:00:44 -0700

    Persistent redis docker-compose

--- services/extract_metadata.py
+++ services/extract_metadata.py
@@ -1,3 +1,4 @@
+#%%
 from models.models import Source
 from services.openai import get_chat_completion
 import json
@@ -5,6 +6,30 @@ from typing import Dict
 import os
 from loguru import logger
 
+from models.models import DocumentMetadata
+from pydantic import BaseModel, Field
+from typing import Any, Dict, List, Optional
+
+# class DocumentMetadata(BaseModel):
+#     source: Optional[Source] = None
+#     source_id: Optional[str] = None
+#     url: Optional[str] = None
+#     created_at: Optional[str] = None
+#     author: Optional[str] = None
+
+#%%
+# schema = DocumentMetadata.schema_json(indent=2)
+# print(schema)
+# def schema_to_function(schema: Any):
+#     # assert schema.__doc__, f"{schema.__name__} is missing a docstring."
+#     return {
+#         "name": schema.__name__,
+#         "description": schema.__doc__.strip(),
+#         "parameters": schema.schema(),
+#     }
+# import orjson
+# print(orjson.dumps(schema_to_function(DocumentMetadata), option=orjson.OPT_INDENT_2).decode())
+# #%%
 def extract_metadata_from_document(text: str) -> Dict[str, str]:
     sources = Source.__members__.keys()
     sources_string = ", ".join(sources)
@@ -28,9 +53,9 @@ def extract_metadata_from_document(text: str) -> Dict[str, str]:
     # NOTE: Azure Open AI requires deployment id
     # Read environment variable - if not set - not used
     completion = get_chat_completion(
-        messages,
-        "gpt-4",
-        os.environ.get("OPENAI_METADATA_EXTRACTIONMODEL_DEPLOYMENTID")
+        messages=messages,
+        model="gpt-4",
+        deployment_id=os.environ.get("OPENAI_METADATA_EXTRACTIONMODEL_DEPLOYMENTID")
     )  # TODO: change to your preferred model name
 
     logger.info(f"completion: {completion}")
